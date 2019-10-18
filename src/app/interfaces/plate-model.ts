862b274845d22d86c78e1f155146b7d286fddf25
swappteam <argesis13@yahoo.com>
2019-10-18 14:25:55 +0300

    remove fcm

---
+++ src/app/interfaces/plate-model.ts
@@ -0,0 +1,8 @@
+
+export interface PlateModel {
+
+  number: string;
+  from: number;
+  to: number;
+
+}
--- src/app/pages/cars/cars.page.ts
+++ src/app/pages/cars/cars.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IonDatetime} from "@ionic/angular";
 
 @Component({
   selector: 'cars',
@@ -8,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
 export class CarsPage implements OnInit {
 
   plates = [];
+  now: Date;
+  tomorrow: Date;
 
   constructor() { }
 
   ngOnInit() {
+    this.now = new Date();
+    this.tomorrow = new Date();
+    this.tomorrow.setDate(this.tomorrow.getDate() + 1);
+  }
+
+  ionViewWillEnter() {
+    this.now = new Date();
+    this.tomorrow = new Date();
+    this.tomorrow.setDate(this.tomorrow.getDate() + 1);
+  }
+
+  addLicensePlate() {
+
   }
 
 }
